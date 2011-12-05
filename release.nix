0dfb708d029aed09ffdfdbcebe66a968e4371054
Nicolas Pierron <nicolas.b.pierron@gmail.com>
2011-12-05 01:50:25 +0100

    Add build rules for Hydra buildfarm.

---
+++ release.nix
@@ -0,0 +1,79 @@
+{ nixpkgs ? <nixpkgs>
+, officialRelease ? false
+}:
+
+let
+  pkgs = import nixpkgs {};
+
+  jobs = rec {
+    tarball =
+      { ionmonkeySrc ? { outPath = <ionmonkey>; }
+      }:
+
+      pkgs.releaseTools.sourceTarball {
+        name = "ionmonkey-tarball";
+        src = ionmonkeySrc;
+        buildInputs = [];
+        autoconf = pkgs.autoconf213;
+        autoconfPhase = ''
+          export VERSION=""
+          export VERSION_SUFFIX=""
+
+          eval "$preAutoconf"
+
+          cd js/src/
+          if test -x ./bootstrap; then ./bootstrap
+          elif test -x ./bootstrap.sh; then ./bootstrap.sh
+          elif test -x ./autogen.sh; then ./autogen.sh
+          elif test -x ./autogen ; then ./autogen
+          elif test -x ./reconf; then ./reconf
+          elif test -f ./configure.in || test -f ./configure.ac; then
+              autoreconf -i -f --verbose
+          else
+              echo "No bootstrap, bootstrap.sh, configure.in or configure.ac. Assuming this is not an GNU Autotools package."
+          fi
+          cd -
+
+          eval "$postAutoconf"
+        '';
+
+        distPhase = ''
+          runHook preDist
+
+          dir=$(basename $(pwd))
+          cd ..
+          ensureDir "$out/tarballs"
+          tar caf "$out/tarballs/$dir.tar.bz2" $dir
+          cd -
+
+          runHook postDist
+        '';
+        inherit officialRelease;
+      };
+
+    jsBuild =
+      { tarball ? jobs.tarball {}
+      , system ? builtins.currentSystem
+      }:
+
+      let pkgs = import nixpkgs { inherit system; }; in
+      pkgs.releaseTools.nixBuild {
+        name = "ionmonkey";
+        src = tarball;
+        postUnpack = ''
+          sourceRoot=$sourceRoot/js/src
+          echo Compile in $sourceRoot
+        '';
+        addInputsHook = ''
+          addToSearchPath PATH ${pkgs.perl}/bin
+          addToSearchPath PATH ${pkgs.python}/bin
+        '';
+        buildInput = with pkgs; [
+          perl python autoconf213 pkgconfig
+        ];
+        configureFlags = [ "--enable-debug" "--disable-optimize" ];
+      };
+  };
+
+in
+  jobs
